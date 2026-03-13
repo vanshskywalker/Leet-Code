9b9b2f121d0a3cd0e6c4190ebbf7bae0882f5e8c
Vansh Chaudhary <vanshskywalker@gmail.com>
2026-03-13 17:18:01 +0530

    Time: 0 ms (100.00%) | Memory: 12.4 MB (48.34%) - LeetSync

---
+++ 119-pascals-triangle-ii/pascals-triangle-ii.py
@@ -0,0 +1,8 @@
+class Solution:
+    def getRow(self, rowIndex):
+        row = [1]
+        for i in range(1, rowIndex + 1):
+            row.append(1)
+            for j in range(i - 1, 0, -1):
+                row[j] += row[j - 1]
+        return row
